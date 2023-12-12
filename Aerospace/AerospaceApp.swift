6423faa9ebe103346cb33ed72e0ea2f554fd9946
Letgatte <samuel.voltolini@icloud.com>
2023-12-12 10:25:25 +0000

    Initial Commit

---
+++ Aerospace/AerospaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  AerospaceApp.swift
+//  Aerospace
+//
+//  Created by Sam Voltolini on 12/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct AerospaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
